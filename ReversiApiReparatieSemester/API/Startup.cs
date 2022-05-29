94d770d129666d668ac1b668fc4919ee6adb77b5
Timdobbel <barco_17>
2022-05-29 15:42:17 +0200

    Update Startup.cs

--- ReversiApiReparatieSemester/API/Startup.cs
+++ ReversiApiReparatieSemester/API/Startup.cs
@@ -31,7 +31,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<SpelContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ReversiDB")), ServiceLifetime.Singleton);
+            services.AddDbContext<SpelContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ReversiDB")), ServiceLifetime.Transient);
 
             services.AddCors(
                 options =>
